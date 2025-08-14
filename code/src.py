addfce48308db8b5f2be6fb3df69a3ad22d95890
huzaifa <huzaifakashif202@gmail.com>
2025-08-15 01:39:04 +0500

    data set transposed and split into dev and train arrays

--- code/src.py
+++ code/src.py
@@ -2,4 +2,21 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
-print("Imports Resolved!")
+data = pd.read_csv('./data/train.csv')
+data = np.array(data)
+
+# m is num of rows and n is num of columns(pixels: 783 total) + 1(label column)
+m, n = data.shape
+# random shuffle data
+np.random.shuffle(data)
+
+# slpit data to dev and train
+data_dev = data[0:1000].T # .T = transpose data to make each pizel a row 
+Y_dev = data_dev[0] # first row
+X_dev = data_dev[1:n] # all columns
+
+data_train = data[1000:m].T
+Y_train = data_train[0]
+X_train = data_train[1:n]
+
+print(X_train[:, 0].shape)
