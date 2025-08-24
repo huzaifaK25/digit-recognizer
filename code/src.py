af17315bc5fc3a95d3b18fe223d82e134c8c0e01
huzaifa <huzaifakashif202@gmail.com>
2025-08-24 18:29:38 +0500

    added functions

--- code/src.py
+++ code/src.py
@@ -19,4 +19,54 @@ data_train = data[1000:m].T
 Y_train = data_train[0]
 X_train = data_train[1:n]
 
-print(X_train[:, 0].shape)
+# initialize values
+
+def init_params():
+    W1 = np.random.rand(10, 784) - 0.5
+    b1 = np.random.rand(10, 1) - 0.5
+    W2 = np.random.rand(10, 10) - 0.5
+    b2 = np.random.rand(10, 1) - 0.5
+    return W1, W2, b1, b2
+
+# if x > 0 then x else 0
+def ReLu(Z):
+    return np.maximum(0, Z)
+
+def deriv_ReLU(Z):
+    return Z > 0
+
+def softmax(Z):
+    return np.exp(Z) / np.sum(np.exp(Z))
+
+def one_hot(Y):
+    one_hot_Y = np.zeros((Y.size, Y.max() + 1))
+    one_hot_Y[np.arange(Y.size), Y] = 1 
+    one_hot_Y = one_hot_Y.T
+    return one_hot_Y
+
+def forward_prop(W1, b1, W2, b2, X):
+    Z1 = W1.dot(X) + b1
+    A1 = ReLu(Z1)
+    Z2 = W2.dot(A1) + b2
+    A2 = softmax(Z2)
+
+def backward_prop(Z1, A1, Z2, A2, W2, X, Y):
+    m = Y.size
+    one_hot_Y = one_hot(Y)
+    dZ2 = A2 - one_hot_Y
+    dW2 = 1/m * dZ2.dot(A1.T)
+    db2 = 1/m * np.sum(dZ2, 2)
+    dZ1 = W2.T.dot(dZ2) * deriv_ReLU(Z1)
+    dW1 = 1/m & dZ1.dot(X.T)
+    db1 = 1/m * np.sum(dZ1, 2)
+    return dW1, db1, dW2, db2
+
+def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
+    W1 = W1 - alpha * dW1
+    b1 = b1 - alpha * db1
+    W2 = W2 - alpha * dW2
+    b2 = b2 - alpha * db2
+    return W1, b1, W2, b2
+
+    
+
